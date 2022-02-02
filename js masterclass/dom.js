c74bbf13965f40b8601c1d6c380c20048b5c4d78
Ashish Shukla <akshkgd@gmail.com>
2022-02-02 22:38:29 +0530

    day 2

--- js masterclass/dom.js
+++ js masterclass/dom.js
@@ -1,16 +1,21 @@
 function changeColor(){
-   document.getElementById('title').style.color = 'red';
+    document.getElementById('title').style.color = 'red'
 }
+
 let counter = 0;
 function increment(){
-  counter = counter + 1;
-  document.getElementById('counter').innerHTML = counter;
+    counter = counter+1;
+    //counter++
+    //counter += 1
+    document.getElementById('counter').innerHTML = counter;
 }
 function decrement(){
-    counter = counter - 1;
+    counter = counter-1;
+    //counter--
+    //counter -= 1
     document.getElementById('counter').innerHTML = counter;
-  }
+}
 function reset(){
     counter = 0;
     document.getElementById('counter').innerHTML = counter;
-  }
+}
---
+++ js masterclass/script/app.js
@@ -0,0 +1,61 @@
+// variables, data types, functions, arrow functions, DOM , Projects -> (ES6)
+
+//variables => let and const  -> (var)
+// data types: string, number, boolean, undefined, null, objects
+
+// let name = "ashish";
+// console.log(typeof name);
+let age = 24.776;
+// console.log(typeof age);
+let isAlive = 'false';
+// console.log(typeof isAlive);
+
+// objects -> Array, objects
+let hobbies = ['cricket', 'football', 'reading'];
+// console.log(hobbies[5]);
+
+
+let user = {
+    name: 'ashish',
+    age: 24,
+    address: '123 main st, chicago, IL',
+    mobile: 123456678899,
+    isAlive: true,
+    userName: 'ashish123',
+}
+user.email = 'ashish@gmail.com';
+delete user.mobile;
+// console.log(user);
+
+
+
+//functions
+
+// function grinder(item){
+//     console.log('grinding....', item);
+// }
+
+// grinder('tomatoes');
+// grinder('sugar, milk, banana');
+
+
+// function cube(num){
+//     console.log(num*num*num);
+// }
+
+// cube(13);
+
+// //arrow functions
+// sqr = (num) => console.log(num*num);
+
+
+// sqr(12)
+
+
+// greet = (name) => console.log('hello ', name);
+
+// let name = prompt('enter your name');
+// greet(name);
+
+
+// DOM
