c00535fca17338bbb674a357be3c89b98469eaad
Ashish Shukla <akshkgd@gmail.com>
2022-08-18 18:40:10 +0530

    js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// es6 => cube counter, bill split application
+// variables, functions, DOM 
+
+console.log('app.js is connected!!')
+
+//variables -> temp. storage  => var, let and const
+let email = 'ashish@gmail.com';
+email = 'yash@gmail.com'
+console.log(email);
+
+let age = 24;
+age = 28;
+console.log(age)
